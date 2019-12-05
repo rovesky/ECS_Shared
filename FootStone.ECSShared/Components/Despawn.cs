81cc0c6e469733ce70ced2d0383a89c8d01eea7c
sam.lu <lsj@com>
2019-12-05 18:58:07 +0800

    代码重构

--- FootStone.ECSShared/Components/Despawn.cs
+++ FootStone.ECSShared/Components/Despawn.cs
@@ -3,7 +3,6 @@ using Unity.Entities;
 
 namespace FootStone.ECS
 {
-    [Serializable]
     public struct Despawn : IComponentData
     {
         public int Frame;
--- FootStone.ECSShared/ReplicatedEntity/ReplicatedEntityDataBehaviour.cs
+++ FootStone.ECSShared/ReplicatedEntity/ReplicatedEntityDataBehaviour.cs
@@ -1,11 +1,16 @@
-﻿using FootStone.ECS;
+﻿using System.Linq;
+using FootStone.ECS;
 using Unity.Entities;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace FootStone.Kitchen
 {
     public class ReplicatedEntityDataBehaviour : MonoBehaviour, IConvertGameObjectToEntity
     {
+
+        public byte[] netID;
 
         void IConvertGameObjectToEntity.Convert(Entity entity, EntityManager dstManager,
             GameObjectConversionSystem conversionSystem)
@@ -18,5 +23,79 @@ namespace FootStone.Kitchen
             });
 
         }
+/*
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            if (EditorApplication.isPlaying)
+                return;
+            SetUniqueNetID();
+
+        }
+
+        //   public static Dictionary<byte[], ReplicatedEntity> netGuidMap = new Dictionary<byte[], ReplicatedEntity>(new ByteArrayComp());
+
+        private void OnValidate()
+        {
+            if (EditorApplication.isPlaying)
+                return;
+
+            SetUniqueNetID();
+
+            //PrefabType prefabType = PrefabUtility.GetPrefabType(this);
+            //if (prefabType == PrefabType.Prefab || prefabType == PrefabType.ModelPrefab)
+            //{
+            //    netID = null;
+            //}
+            //else
+            //    SetUniqueNetID();
+
+            //UpdateAssetGuid();
+        }
+
+        //public bool SetAssetGUID(string guidStr)
+        //{
+        //    var guid = new WeakAssetReference(guidStr);
+        //    var val = Value;
+        //    var currentGuid = val.assetGuid;
+        //    if (!guid.Equals(currentGuid))
+        //    {
+        //        val.assetGuid = guid;
+        //        Value = val;
+        //        PrefabUtility.SavePrefabAsset(gameObject);
+        //        return true;
+        //    }
+
+        //    return false;
+        //}
+
+        //public void UpdateAssetGuid()
+        //{
+        //    // Set type guid
+        //    var stage = PrefabStageUtility.GetPrefabStage(gameObject);
+        //    if (stage != null)
+        //    {
+        //        var guidStr = AssetDatabase.AssetPathToGUID(stage.prefabAssetPath);
+        //        if (SetAssetGUID(guidStr))
+        //            EditorSceneManager.MarkSceneDirty(stage.scene);
+        //    }
+        //}
+
+        private void SetUniqueNetID()
+        {
+            
+            // Generate new if fresh object
+            if (netID == null || netID.Length == 0)
+            {
+                var guid = gameObject.name;
+                netID = guid.ToArray<byte>();
+                EditorSceneManager.MarkSceneDirty(gameObject.scene);
+            }
+
+        }
+
+#endif
+*/
+
     }
 }
